eaa1bd0ff68c46a5ab24dac7bf0357fe529859bf
ThanhKieu <hanguyenthanhkieu12345dl@gmail.com>
2026-06-24 11:55:38 +0700

    Tui da lam phan giao dien cua 3 file xml

--- app/src/main/java/com/minhhuycoder/vidi/core/PlaceAdapter.kt
+++ app/src/main/java/com/minhhuycoder/vidi/core/PlaceAdapter.kt
@@ -1,19 +1,22 @@
 package com.minhhuycoder.vidi.core
 
+import android.widget.ImageView
 import android.widget.TextView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.minhhuycoder.vidi.models.PlaceModel
 import com.minhhuycoder.vidi.R
 
 class PlaceAdapter(private val placeList: List<PlaceModel>) : RecyclerView.Adapter<PlaceAdapter.PlaceViewHolder>() {
 
     class PlaceViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        // Khai báo các view trong item_place.xml ở đây
-        val tvName: TextView = itemView.findViewById(R.id.tvName) // Sửa ID theo XML của mày
+        val tvName: TextView = itemView.findViewById(R.id.tvPlaceName)
         val tvAddress: TextView = itemView.findViewById(R.id.tvAddress)
+        val tvRating: TextView = itemView.findViewById(R.id.tvRating)
+        val ivPlaceImage: ImageView = itemView.findViewById(R.id.ivPlaceImage)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceViewHolder {
@@ -25,8 +28,12 @@ class PlaceAdapter(private val placeList: List<PlaceModel>) : RecyclerView.Adapt
         val place = placeList[position]
         holder.tvName.text = place.name
         holder.tvAddress.text = place.address
+        holder.tvRating.text = "${place.ratingAverage} ★"
 
-        // holder.tvDescription.text = place.description
+        Glide.with(holder.itemView.context)
+            .load(place.imageUrl)
+            .placeholder(android.R.drawable.ic_menu_gallery)
+            .into(holder.ivPlaceImage)
     }
 
     override fun getItemCount() = placeList.size
